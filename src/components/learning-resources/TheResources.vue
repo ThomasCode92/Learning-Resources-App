3a003654ffff41de159bb0152be6b7db4c35d8d9
ThomasCode92 <thomascode92@hotmail.com>
2022-09-07 12:31:27 +0200

    Adding a Modal Dialog

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -13,7 +13,9 @@
       Add Resource
     </base-button>
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
